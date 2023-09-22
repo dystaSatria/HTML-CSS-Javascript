3a40b00ac41ad84f7482fa3f6d8f5981759bb4ec
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-09-22 15:24:31 -0400

    Create Variable.js

---
+++ JavaScript Mini Task/onlinePlatformMiniTask/Variable.js
@@ -0,0 +1,15 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai "John".
+ *  - lastName: bertipe data string, dengan nilai "Doe".
+ *  - age: bertipe data number, dengan nilai 25.
+ *  - isMarried: bertipe data boolean, dengan nilai true.
+ */
+
+// Tulis kode di bawah ini
+
+var firstName = "John";
+var lastName = "Doe";
+var age = 25;
+var isMarried = true;
